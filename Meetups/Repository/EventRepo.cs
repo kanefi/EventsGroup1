ca60c16a8f3805bad4583657e6b5bebf24bb4a39
FKANE <FKANE@SBWT618303>
2020-04-21 15:31:13 +0100

    Added tables and updated repos

---
+++ Meetups/Repository/EventRepo.cs
@@ -0,0 +1,23 @@
+﻿using Meetups.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Meetups.Repository
+{
+    public class EventRepo : IEventRepo
+    {
+
+        public string _cnnstring = "server=db-workshop.public-dev.zuto.cloud;user=bootcamp6;database=bootcamp6;port=3306;password=SimpleKettle";
+
+
+        public Task<Event> GetEventsAsync()
+        {
+            
+
+        }
+    }
+
+}
